7d89c0ecf23fdcf45095f7aa1edaa8a2efb0887e
Zdravko Zdravkov <zdr0@yahoo.com>
2022-08-30 13:27:13 +0200

    Singletons

--- 02. Design Patterns - Creational Patterns/Creational Patterns/01. Singleton/Program.cs
+++ 02. Design Patterns - Creational Patterns/Creational Patterns/01. Singleton/Program.cs
@@ -1,16 +1,27 @@
-﻿namespace Singleton
+﻿namespace Singleton;
+
+using Singleton.Singletons;
+
+using System;
+
+public static class Program
 {
-    using System;
-
-    public static class Program
+    public static void Main()
     {
-        public static void Main()
-        {
-            var logger = LoggerThreadSave.Instance;
-            var secondLogger = LoggerThreadSave.Instance;
+        Singleton<LoggerGenericLazyImplementation>();
+    }
 
-            Console.WriteLine(logger == secondLogger);
-            Console.WriteLine(logger.GetHashCode() == secondLogger.GetHashCode());
+    private static void Singleton<T>() 
+        where T : ILogger<T>
+    {
+        for (var i = 1; i <= 12; i++)
+        {
+            T.Instance.Queue.Add($"Log: {i}");
+        }
+
+        foreach (var log in T.Instance.Queue)
+        {
+            Console.WriteLine(log);
         }
     }
 }
