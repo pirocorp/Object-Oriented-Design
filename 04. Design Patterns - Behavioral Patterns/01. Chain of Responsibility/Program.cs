e212151e8e34ab7b767fffb0b20f03286cacd7a7
Zdravko Zdravkov <zdr0@yahoo.com>
2022-11-28 18:39:47 +0100

    Chain of Responsibility

--- 04. Design Patterns - Behavioral Patterns/01. Chain of Responsibility/Program.cs
+++ 04. Design Patterns - Behavioral Patterns/01. Chain of Responsibility/Program.cs
@@ -1,8 +1,17 @@
 ﻿namespace ChainOfResponsibility
 {
+    using Corporate;
+    using Loggers;
+
     public static class Program
     {
         public static void Main()
+        {
+            // CorporateDemo();
+            LoggerDemo();
+        }
+
+        private static void CorporateDemo()
         {
             var ronny = new Director();
             var bobby = new VicePresident();
@@ -19,6 +28,28 @@
 
             p = new Purchase(3256, 144400.00M, "Project BBD");
             ronny.ProcessRequest(p);
+        }
+
+        private static void LoggerDemo()
+        {
+            // Build the chain of responsibility
+            var logger = new ConsoleLogger(LogLevel.All)
+                .SetNext(new EmailLogger(LogLevel.FunctionalMessage | LogLevel.FunctionalError))
+                .SetNext(new FileLogger(LogLevel.Warning | LogLevel.Error));
+
+            // Handled by ConsoleLogger since the console has a logLevel of all
+            logger.Message("Entering function ProcessOrder().", LogLevel.Debug);
+            logger.Message("Order record retrieved.", LogLevel.Info);
+
+            // Handled by ConsoleLogger and FileLogger since fileLogger implements Warning & Error
+            logger.Message("Customer Address details missing in Branch DataBase.", LogLevel.Warning);
+            logger.Message("Customer Address details missing in Organization DataBase.", LogLevel.Error);
+
+            // Handled by ConsoleLogger and EmailLogger as it implements functional error
+            logger.Message("Unable to Process Order ORD1 Dated D1 For Customer C1.", LogLevel.FunctionalError);
+
+            // Handled by ConsoleLogger and EmailLogger
+            logger.Message("Order Dispatched.", LogLevel.FunctionalMessage);
         }
     }
 }
