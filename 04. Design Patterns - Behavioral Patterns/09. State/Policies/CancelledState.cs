cce3e7a362958654088cc0d55a9d9209d6d3b5c2
Zdravko Zdravkov <zdr0@yahoo.com>
2022-11-29 18:07:58 +0100

    09. State - documentation

--- 04. Design Patterns - Behavioral Patterns/09. State/Policies/CancelledState.cs
+++ 04. Design Patterns - Behavioral Patterns/09. State/Policies/CancelledState.cs
@@ -3,6 +3,15 @@
 using System;
 using System.Collections.Generic;
 
+/// <summary>
+/// Note that in this example each State subtype is defined as an
+/// inner class within the Policy class. The classes are defined
+/// in separate partial Policy classes so they can reside in separate
+/// files without bloating the Policy class definition. Since these
+/// State classes are defined as inner classes, they have direct
+/// access to private member variables defined in Policy, so there
+/// is no need to expose Policy's list of possible states publicly.
+/// </summary>
 public partial class Policy
 {
     public class CancelledState : IPolicyStateCommands
