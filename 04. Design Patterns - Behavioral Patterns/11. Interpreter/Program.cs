605f726c47aa612e9e87e1ddc257d442465459a2
Zdravko Zdravkov <zdr0@yahoo.com>
2022-11-30 10:38:12 +0100

    11. Interpreter

---
+++ 04. Design Patterns - Behavioral Patterns/11. Interpreter/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Interpreter;
+
+using System;
+using RomanNumbers;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var romanString = "MCMXXVIII";
+        var roman = new RomanNumber(romanString);
+
+        Console.WriteLine($"{romanString} = {roman.Value}");
+        Console.WriteLine($"{romanString} = {roman.Value}");
+    }
+}
---
+++ 04. Design Patterns - Behavioral Patterns/11. Interpreter/RomanNumbers/RomanNumber.cs
@@ -0,0 +1,52 @@
+﻿namespace Interpreter.RomanNumbers;
+
+using System.Collections.Generic;
+
+public class RomanNumber
+{
+    private readonly string romanNumber;
+    private readonly Context context;
+    private readonly List<Expression> tree;
+
+    private int value;
+
+    public RomanNumber(string romanNumber)
+    {
+        this.romanNumber = romanNumber;
+        this.context = new Context(romanNumber);
+        this.tree = new List<Expression> ();
+
+        this.Init();
+    }
+
+    public int Value 
+        => this.context.Input == string.Empty 
+            ? this.value 
+            : this.CalculateValue();
+
+    /// <summary>
+    /// Interpret expression
+    /// </summary>
+    /// <returns>Interpreted value</returns>
+    private int CalculateValue()
+    {
+        foreach (var exp in tree)
+        {
+            exp.Interpret(context);
+        }
+
+        this.value = this.context.Output;
+        return this.value;
+    }
+
+    /// <summary>
+    /// Build the 'parse tree'
+    /// </summary>
+    private void Init()
+    {
+        tree.Add(new ThousandExpression());
+        tree.Add(new HundredExpression());
+        tree.Add(new TenExpression());
+        tree.Add(new OneExpression());
+    }
+}
